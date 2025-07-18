dc845685cdc4c2c62c010aff900b508045db1ac2
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-07-17 22:03:05 -0400

    Menú agregado

--- evaluacion-appnotas.py
+++ evaluacion-appnotas.py
@@ -1,7 +1,8 @@
-tareas = [{"nombre": "Estudiar documentación", "estado": False}, {"nombre": "Hacer actividad de portafolio", "estado": False}]
+tareas = [{"nombre": "Estudiar documentación", "estado": False}, {"nombre": "Hacer actividad de portafolio", "estado": False}, {"nombre": "Alimentar a Titto el Gatito Gordito", "estado": True}]
 
 def agregarTarea(tarea):
-    tareas.append(tarea)
+    tareaAAgregar = {"nombre": tarea, "estado": False}
+    tareas.append(tareaAAgregar)
 
 def listarTareas():
     ancho = 65
@@ -12,12 +13,49 @@ def listarTareas():
         print(f'{i + 1}. {tarea["nombre"]}{relleno}{estadoTarea}')
 
 
-def marcarCompletada(indice):
+def marcarTarea(indice):
     indice -= 1
     tareas[indice]["estado"] = not tareas[indice]["estado"]
 
+def eliminarTarea(indice):
+    tareas.pop(indice-1)
+
+#  Programa
+
+opcion = 0
 
 
-listarTareas()
-marcarCompletada(1)
-listarTareas()
+
+while True:
+    print("""
+          --- Gestor de Tareas ---
+          1. Agregar tarea
+          2. Ver tareas
+          3. Marcar tarea como Completada/Pendiente
+          4. Eliminar tarea
+          5. Salir""")
+    try:
+        opcion = int(input("Elige una opción: "))
+    except:
+        print("*************Debe seleccionar una opción válida*************")
+    if opcion == 1:
+        tarea = input("Ingrese el nombre de la tarea")
+        agregarTarea(tarea)
+        print(f"¡'{tarea} agregada exitosamente!'")
+    if opcion == 2:
+        listarTareas()
+    if opcion == 3:
+        listarTareas()
+        indice = int(input("Ingrese el número de la tarea a completar o dejar pendiente: "))
+        marcarTarea(indice)
+        listarTareas()
+    if opcion == 4:
+        listarTareas()
+        indice = int(input("Ingrese el número de la tarea a eliminar: "))
+        tareaAEliminar = tareas[indice]["nombre"]
+        eliminarTarea(indice)
+        print(f"¡'{tareaAEliminar}' eliminada exitosamente!")
+    if opcion == 5:
+        break
+
+print("¡Gracias por utilizar el Gestor de tareas! ¡Hasta pronto!")
